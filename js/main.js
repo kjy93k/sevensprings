8cb21642374cc3adffb696df2b00c4213957e7f2
kjy93k <46341006+kjy93k@users.noreply.github.com>
2019-01-18 17:23:44 +0900

    20180118

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	var $current=0;
+	//var $noticeeq = eval($current-1);
+	var $prev= $('.left.png');
+	var $next= $('.right.png');
+
 	$('.gnb>ul ul').hide();
 	$('.main-visual').bxSlider({
 		auto: true,
@@ -28,6 +33,16 @@ $(document).ready(function(){
 		$('#header .gnb-down').fadeOut(350)
 	});
 		*/
+	/*
+	$('.content-1>ul>li').hover(function(){
+		var $teq = $(this).index();
+		var $burl = 'url(images/main/c1-'+eval($teq+1)+'h.png)';
+		var $bburl = 'url(images/main/c1-'+eval($teq+1)+'.png)';
+		$(this).css('background',$burl);
+	},function(){
+		$(this).css('background',$bburl);
+	});
+	*/
 	$('.ct2Btn').click(function(){
 		var $i =$(this).parent();
 		var $ii = $(this).parent().parent().children('.content-2-txt-wrap1');
@@ -35,4 +50,41 @@ $(document).ready(function(){
 		$(this).toggleClass('on');
 		$ii.fadeToggle(800);
 	});
+	
+	$('.content-3-gallery > ul > li').hide();
+	$('.content-3-gallery > ul > li').eq(0).show();
+	$('.thumbnail>ul>li').on('click',function(){
+		$('.content-3-gallery > ul > li').hide();
+		var $i = $(this).index();
+		$current=$i;
+		$('.content-3-gallery > ul > li').eq($i).show();
+		$('.thumbnail>ul>li').removeClass('on');
+		$(this).addClass('on');
+	});
+	$('.content-3-gallery .left').on('click',function(){
+		if($current>0){
+			$current--;
+		} else {
+			$current=7;
+		}
+		//$('.notice-inner-wrap>p>img').attr('src','images/main/event-'+$current+'.jpg')
+		$('.content-3-gallery>ul>li').hide();
+		$('.content-3-gallery>ul>li').eq($current).show();
+		$('.thumbnail>ul>li').removeClass('on');
+		$('.thumbnail>ul>li').eq($current).addClass('on');
+	});
+	$('.content-3-gallery .right').on('click',function(){
+		if($current<7){
+			$current++;
+		} else {
+			$current=0;
+		}
+		//$('.notice-inner-wrap>p>img').attr('src','images/main/event-'+$current+'.jpg')
+		$('.content-3-gallery>ul>li').hide();
+		$('.content-3-gallery>ul>li').eq($current).show();
+		$('.thumbnail>ul>li').removeClass('on');
+		$('.thumbnail>ul>li').eq($current).addClass('on');
+	});
+/*	
+	*/
 });
